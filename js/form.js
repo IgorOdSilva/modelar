24c9d5c1bbb576395c4b69de92e17715c76a4b53
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2024-05-05 21:32:55 -0300

    ajustes finais

--- js/form.js
+++ js/form.js
@@ -14,50 +14,19 @@ const handleSubmit = (event) => {
     const nome = document.querySelector('input[name=nome]').value;
     const email = document.querySelector('input[name=email]').value;
     const numero = document.querySelector('input[name=numero]').value;
+    const apiKey = '20612e8d-2f51-4c07-a735-c37d8cc11f37'
 
     // Envia os dados para a API
     fetch('https://api.sheetmonkey.io/form/qu2GQ1m7qFKowz5H2Nk6TP', {
         method: 'post',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json', 
         },
         body: JSON.stringify({ nome, email, numero }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Ocorreu um erro ao enviar os dados para a API.');
-        }
-        // Se o envio para a API for bem-sucedido, envie o email
-        return Email.send({
-            Host: "smtp.gmail.com",
-            Username: "igornutrietec2020@gmail.com", // Substitua com seu endereço de email
-            Password: "210510igor", // Substitua com sua senha
-            To: 'oliveiradasilvaigor74@gmail.com',
-            From: "igornutrietec2020@gmail.com", // Substitua com seu endereço de email
-            Subject: "Cliente Cadastrado - NewsLetter",
-            Body: `Nome: ${nome}\nEmail: ${email}\nNúmero: ${numero}`,
-        });
-    })
-    .then(() => {
+    }).then(() => {
         removeLoading();
     })
-    .then((message) => {
-        console.log("Resposta do servidor de email:", message);
-        if (message === "OK" || message === undefined) {
-            // Email enviado com sucesso
-            console.log("Email enviado com sucesso!");
-            removeLoading();
-        } else {
-            // Ocorreu um erro ao enviar o email
-            console.error("Ocorreu um erro ao enviar o email:", message);
-            // Lide com o erro aqui
-        }
-    })
-    .catch(error => {
-        console.error('Ocorreu um erro:', error);
-        // Lide com o erro aqui
-    });
 }
 
 
