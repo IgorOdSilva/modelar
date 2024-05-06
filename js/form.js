eb2f1f52c4940da3ea5461e685c50cd50050a1c5
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2024-05-06 10:40:01 -0300

    newsletter finalizada

--- js/form.js
+++ js/form.js
@@ -8,13 +8,52 @@ const removeLoading = () => {
     document.getElementById('form').style.display = 'none';
 }
 
+const sendEmail = () => {
+    const formSpreeUrl = 'https://formspree.io/f/mkndykyn'; // Substitua pelo seu endereço de e-mail do Formspree
+
+    const nome = document.querySelector('input[name=nome]').value;
+    const email = document.querySelector('input[name=email]').value;
+    const numero = document.querySelector('input[name=numero]').value;
+
+    const emailBody = `
+        Nome: ${nome}
+        Email: ${email}
+        Número: ${numero}
+    `;
+
+    fetch(formSpreeUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: emailBody, // Corpo do e-mail
+            _subject: 'Novo usuário cadastrado Newsletter', // Assunto do e-mail
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao enviar o formulário');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        // Faça algo após o envio do formulário, se necessário
+    })
+    .catch(error => {
+        console.error('Erro ao enviar o formulário:', error);
+    });
+}
+
+
+
 const handleSubmit = (event) => {
     event.preventDefault();
     addLoading();
     const nome = document.querySelector('input[name=nome]').value;
     const email = document.querySelector('input[name=email]').value;
     const numero = document.querySelector('input[name=numero]').value;
-    const apiKey = '20612e8d-2f51-4c07-a735-c37d8cc11f37'
 
     // Envia os dados para a API
     fetch('https://api.sheetmonkey.io/form/qu2GQ1m7qFKowz5H2Nk6TP', {
@@ -24,6 +63,8 @@ const handleSubmit = (event) => {
             'Content-Type': 'application/json', 
         },
         body: JSON.stringify({ nome, email, numero }),
+    }).then(() => {
+        sendEmail();
     }).then(() => {
         removeLoading();
     })
