99d69c62b6ea7a5d795cda5b81497218e9ba8b74
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2024-05-12 10:10:49 -0300

    ajuste carrossel
    pedido cliente ajuste carrossel adicionar pagination e botão

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,6 +1,17 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-    allowTouchMove: true // Permitir navegação por clique e arrastar
+    allowTouchMove: true, // Permitir navegação por clique e arrastar
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
 });
   
